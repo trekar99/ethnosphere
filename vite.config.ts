db9dade4e310ce19345b95a9e903146a74f43e06
trekar99 <101529212+trekar99@users.noreply.github.com>
2026-02-09 15:07:33 +0100

    Update base URL for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import cesium from 'vite-plugin-cesium'
 
-// https://vite.dev/config/
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
   plugins: [react(), cesium()],
-
-  base: "/EthnoSphere/",
+  base: isGitHubPages ? "/EthnoSphere/" : "/", 
 })
